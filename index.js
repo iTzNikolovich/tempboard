060d2bf5f717e99f80e1561c9af08a6454d20da7
Nicolò Carcagnì <nicolo.carcagni@mail.com>
2023-07-28 21:50:35 +0200

    v1.1.0

--- index.js
+++ index.js
@@ -12,33 +12,34 @@ const { KVBase } = require('./KVBase');
 let db = new KVBase();
 
 app.get('/', (req, res) => {
-  res.send('TempBoard v1.0 is running!')
+  res.send('TempBoard v1.1 is running!')
 });
 
 app.post('/up', async (req, res) => {
   // get data from the sensor
   const mac = req.body.mac;
   const temp = req.body.temp;
+  const hum = req.body.hum; // To disable humidity support comment this line
 
   // store data in db
   ts = Date.now();
-  var data = { mac: mac, date: ts, temp: temp };
-  //console.log(data);
+  var data = { mac: mac, date: ts, temp: temp, hum: hum }; // To disable humidity support delete 'hum: hum'
+  console.log(data);
   await db.set(data.mac, data);
-  res.send('TempBoard v1.0: Success!')
+  res.send('TempBoard v1.1: Success!')
 });
 
-app.get('/:mac', async (req, res) => {
+app.get('/get/:mac', async (req, res) => {
   const mac = req.params.mac;
   if (mac != '') {
     var data = await db.get(mac);
-    //console.log('TempBoard v1.0: Success!');
+    //console.log('TempBoard v1.1: Success!');
     res.send(data)
   } else {
-    res.send('TempBoard v1.0: not enough parameters...')
+    res.status(404).send('not found')
   }
 });
 
 app.listen(3000, () => {
-  console.log('TempBoard v1.0 is running!');
+  console.log('TempBoard v1.1 is running!');
 });
